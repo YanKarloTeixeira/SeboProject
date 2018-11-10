33714b9f4dae7039909e83d514b8cf843b5447ba
Yan Karlo Teixeira <yankarlo@hotmail.com>
2018-11-10 14:35:55 -0500

    Localization - Search + DropBox Filters + Columns Ordering Features Introduction

--- SeboProject/Controllers/LocalizationsController.cs
+++ SeboProject/Controllers/LocalizationsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SeboProject.Data;
 using SeboProject.Models;
+using SeboProject.Utilities;
 
 namespace SeboProject.Controllers
 {
@@ -20,9 +21,81 @@ namespace SeboProject.Controllers
         }
 
         // GET: Localizations
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Localization.ToListAsync());
+        //}
+        public async Task<IActionResult> Index(string sortOrder, string currentSearchString, string SearchString, int ProvinceFilter, int PlaceNameFilter, int? Page)
         {
-            return View(await _context.Localization.ToListAsync());
+            int PageSize = 14; // How many listed items per page definition
+            var seboDbContext = _context.Localization;
+            var BodyList = (from p in seboDbContext select p); // Item to be listed on the view
+
+            if (!String.IsNullOrEmpty(sortOrder))
+            {
+                string[] parameter = sortOrder.Split(".");
+                sortOrder = parameter[0];
+                ProvinceFilter = Int32.Parse(parameter[1]);
+                PlaceNameFilter = Int32.Parse(parameter[2]);
+            }
+
+
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                Page = 1;
+                BodyList = BodyList.Where(p => p.PostalCode.Contains(SearchString) || p.Province.Contains(SearchString) ||
+                                                                                              p.PlaceName.Contains(SearchString));
+            }
+            else currentSearchString = SearchString;
+
+            // Applying text filters on the table - It impacts in all dropboxes and lists
+            
+            //Initializing the queries that belong to the dropboxes
+            var _PlaceNamesFiltered = BodyList;
+            var _ProvincesFiltered = BodyList;
+
+            // Applying filter over the dropbox query base
+            if (ProvinceFilter != 0)
+            {
+                _PlaceNamesFiltered = from p1 in _PlaceNamesFiltered join p2 in _PlaceNamesFiltered on p1.Province equals p2.Province
+                                      where (p1.LocalizationId == ProvinceFilter) select p2;
+            }
+
+            /*
+                Adjusts the BodyList according to the Province selection 
+                and the PlaceName selection 
+            */ 
+            BodyList = _PlaceNamesFiltered;
+            if (PlaceNameFilter != 0 ) BodyList = _PlaceNamesFiltered.Where(p => p.LocalizationId == PlaceNameFilter).Distinct();
+            if (BodyList.Count() < 1) BodyList = _PlaceNamesFiltered; // In case no match for Province+PlaceName 
+
+            
+
+            //_PlaceNamesFiltered = OrderingPostalCodes.Do(_PlaceNamesFiltered, sortOrder);
+            /* Ordering before list on the view */
+            BodyList = OrderingPostalCodes.Do(BodyList, sortOrder);
+
+            /*
+                Preparing DROPBOXLISTs
+            */
+            var _Provinces = (from p in _ProvincesFiltered group p by p.Province into pp select pp.First() ).ToList();
+            var _PlaceNames = (from p in _PlaceNamesFiltered orderby p.PlaceName select new { p.LocalizationId, p.PlaceName }).ToList().Distinct();
+
+            ViewData["ProvinceFilter"] = new SelectList(_Provinces, "LocalizationId", "Province");
+            ViewData["PlaceNameFilter"] = new SelectList(_PlaceNames, "LocalizationId", "PlaceName");
+            /* END of (Preparing DROPBOXLISTs) */
+
+            ViewData["SearchString"] = SearchString;
+            /*
+             * Adding actual dropboxes selection because in case of a column ordering (by clicking on the column name) 
+             * the dropbox filtering values would be lost.
+             */
+            ViewData["ProvinceOrder"] = OrderingPostalCodes.NewOrder(sortOrder, "Province") +"." + ProvinceFilter+ "."+ PlaceNameFilter;
+            ViewData["PlaceNameOrder"] = OrderingPostalCodes.NewOrder(sortOrder, "PlaceName")+"." + ProvinceFilter + "." + PlaceNameFilter;
+            ViewData["PostalCodeOrder"] = OrderingPostalCodes.NewOrder(sortOrder, "PostalCode")+"." + ProvinceFilter + "." + PlaceNameFilter;
+
+            return View(await Pagination<Localization>.CreateAsync(BodyList.AsNoTracking(), Page ?? 1, PageSize));
+
         }
 
         // GET: Localizations/Details/5
---
+++ SeboProject/Utilities/OrderingPostalCodes.cs
@@ -0,0 +1,70 @@
+﻿using SeboProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeboProject.Utilities
+{
+    public class OrderingPostalCodes
+    {
+        public static IQueryable<Localization> Do(IQueryable<Localization> PostalCodes, string sortOrder)
+        {
+            switch (sortOrder)
+            {
+                case "PostalCode_asc":
+                    PostalCodes = PostalCodes.OrderByDescending(p => p.PostalCode);
+                    break;
+                case "PostalCode_desc":
+                    PostalCodes = PostalCodes.OrderBy(p => p.PostalCode);
+                    break;
+                case "Province_asc":
+                    PostalCodes = PostalCodes.OrderByDescending(p => p.Province).ThenBy(p => p.PlaceName);
+                    break;
+                case "Province_desc":
+                    PostalCodes = PostalCodes.OrderBy(p => p.Province).ThenBy(p => p.PlaceName);
+                    break;
+                case "PlaceName_asc":
+                    PostalCodes = PostalCodes.OrderByDescending(p => p.PlaceName);
+                    break;
+                case "PlaceName_desc":
+                    PostalCodes = PostalCodes.OrderBy(p => p.PlaceName);
+                    break;
+                default:
+                    PostalCodes = PostalCodes.OrderBy(p => p.Province).ThenBy(p => p.PlaceName);
+                    break;
+            }
+            return PostalCodes;
+
+        }
+
+        public static string NewOrder(string SortOrder, string ColumnName)
+        {
+            string NewOrder = null;
+            switch (ColumnName)
+            {
+
+                case "PostalCode":
+                    if (SortOrder == "PostalCode_asc")
+                        NewOrder = "PostalCode_desc";
+                    else
+                        NewOrder = "PostalCode_asc";
+                    break;
+                case "Province":
+                    if (SortOrder == "Province_asc")
+                        NewOrder = "Province_desc";
+                    else
+                        NewOrder = "Province_asc";
+                    break;
+                case "PlaceName":
+                    if (SortOrder == "PlaceName_asc")
+                        NewOrder = "PlaceName_desc";
+                    else
+                        NewOrder = "PlaceName_asc";
+                    break;
+            }
+            return NewOrder;
+        }
+
+    }
+}
